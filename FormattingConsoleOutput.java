423098b39f2d7a93774e437b7faeb4e3afa1ea99
noname65 <57306016+noname65@users.noreply.github.com>
2019-12-22 22:12:44 +0800

    Add files via upload

---
+++ FormattingConsoleOutput.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formattingconsoleoutput;
+
+/**
+ *
+ * @author Administrator
+ */
+public class FormattingConsoleOutput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String text = "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n" +
+"@\n" +
+"@                          r-p-o-p-h-e-s-s-a-g-r\n" +
+"@                       E. E. Cummings - 1894-1962\n" +
+"@\n" +
+"@\n" +
+"@\n" +
+"@                                     r-p-o-p-h-e-s-s-a-g-r\n" +
+"@                           who\n" +
+"@  a)s w(e loo)k\n" +
+"@  upnowgath\n" +
+"@                       PPEGORHRASS\n" +
+"@                                                       eringint(o-\n" +
+"@  aThe):l\n" +
+"@               eA\n" +
+"@                    !p:\n" +
+"@ S                                                                        a\n" +
+"@                                     (r\n" +
+"@ rIvInG                              .gRrEaPsPhOs)\n" +
+"@                                                                        to\n" +
+"@ rea(be)rran(com)gi(e)ngly\n" +
+"@ ,grasshopper;\n" +
+"@\n" +
+"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\";
+        System.out.println(text);
+    }
+    
+}
